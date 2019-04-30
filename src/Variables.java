329efd6041862cd9e6e0b06ee9e869ad61e677a4
Dell <dandan_21@o2.pl>
2019-04-30 15:07:38 +0200

    stworzenie nowej klasy Variables do przechowywania zmiennych i operacjach na nich

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Jan Kowalski ma 25 lat");
+        System.out.println("PESEL Jana Kowalskiego to 1234567890" );
+        System.out.println("Jan Kowalski ma 180 cm wzrostu");
+        System.out.println("Jesli Jan urosnie o 5 cm bedzie mial 185 cm wzrostu");
+    }
+}
