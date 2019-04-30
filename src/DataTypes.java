bb6f682e29bfcce1cd1d787b4c6c8a68cddd60ac
Dell <dandan_21@o2.pl>
2019-04-30 16:09:57 +0200

    dodanie klasy DataTypes zawierajacej operacje na typach danych

---
+++ src/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println(100000000000000000L);
+
+        System.out.println("Ala ma kota");
+
+        System.out.println(5.55555F);
+        System.out.println(1.0/3.0);
+        System.out.println(1.0f/3.0f);
+
+        System.out.println('a');
+        System.out.println('\u00AE');
+        System.out.println(false);
+        System.out.print("Hallo dzien dobry pakazmy przejscie do nowej lini\n");
+        System.out.print("(12345)\n");
+        System.out.print("Witaj\t\"Swiecie\"");
+    }
+}
