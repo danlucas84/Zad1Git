93ddf7313fdf615a2e901d0e4a1d7c3b73abcd75
Dell <dandan_21@o2.pl>
2019-04-30 15:35:46 +0200

    dodanie konkatanacji stringów w klasie Variables

--- src/Variables.java
+++ src/Variables.java
@@ -3,13 +3,19 @@ public class Variables {
 
         String firstName = "Jan";
         String lastName = "Kowalski";
-        int age;
-        double height;
-        String pesel;
+        int age = 35;
+        double height = 185.50;
+        String pesel = "1234567890";
 
-        System.out.println("Jan Kowalski ma 25 lat");
+        /*System.out.println("Jan Kowalski ma 25 lat");
         System.out.println("PESEL Jana Kowalskiego to 1234567890" );
         System.out.println("Jan Kowalski ma 180 cm wzrostu");
         System.out.println("Jesli Jan urosnie o 5 cm bedzie mial 185 cm wzrostu");
+        */
+        System.out.println(firstName + " " + lastName + " ma " + age + " lat");
+        System.out.println(firstName + " " + lastName + " ma " + height + " cm wzrostu");
+        System.out.println("Jesli " + firstName + " urosnie o 5 cm  bedzie miał "+ (height+5) + " cm wzrostu" );
+
+
     }
 }
